0bed110b2a4d793d259141b64319eaa8665158b1
Landini, DA <landinialejandro@hotmail.com>
2026-03-20 16:24:22 -0300

    Convierte el calendario mensual en un bloque con scroll interno, cabecera sticky y columna de semana compacta

--- resources/views/appointments/partials/calendar-grid.blade.php
+++ resources/views/appointments/partials/calendar-grid.blade.php
@@ -2,28 +2,30 @@
     $weekdays = ['Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sáb', 'Dom'];
 @endphp
 
-<div class="appointment-calendar">
-    <div class="appointment-calendar-head appointment-calendar-head--with-week">
-        <div class="appointment-calendar-weeknumber-head">
-            Sem.
+<div class="appointment-calendar-scroll">
+    <div class="appointment-calendar">
+        <div class="appointment-calendar-head appointment-calendar-head--with-week">
+            <div class="appointment-calendar-weeknumber-head">
+                Sem.
+            </div>
+
+            @foreach ($weekdays as $weekday)
+                <div class="appointment-calendar-weekday">
+                    {{ $weekday }}
+                </div>
+            @endforeach
         </div>
 
-        @foreach ($weekdays as $weekday)
-            <div class="appointment-calendar-weekday">
-                {{ $weekday }}
-            </div>
-        @endforeach
-    </div>
+        <div class="appointment-calendar-body appointment-calendar-body--with-week">
+            @foreach ($weeks as $week)
+                <div class="appointment-calendar-weeknumber">
+                    {{ $week['week_number'] }}
+                </div>
 
-    <div class="appointment-calendar-body appointment-calendar-body--with-week">
-        @foreach ($weeks as $week)
-            <div class="appointment-calendar-weeknumber">
-                {{ $week['week_number'] }}
-            </div>
-
-            @foreach ($week['days'] as $day)
-                @include('appointments.partials.calendar-day-cell', ['day' => $day])
+                @foreach ($week['days'] as $day)
+                    @include('appointments.partials.calendar-day-cell', ['day' => $day])
+                @endforeach
             @endforeach
-        @endforeach
+        </div>
     </div>
 </div>
